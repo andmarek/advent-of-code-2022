522210a3ec49dd057ffd3ac40321d601940cb5b8
Andrew <andwmarek@gmail.com>
2022-12-08 22:28:04 -0600

    6 done

---
+++ 6/sol.py
@@ -0,0 +1,11 @@
+PACKET = 4
+MESSAGE = 14
+
+def find_start_of(distinct_characters: int):
+    with open("data", "r") as f:
+        data = f.read()
+        for i in range(len(data)-distinct_characters-1): 
+            if len(data[i:i+distinct_characters]) == len(set(list(data[i:i+distinct_characters]))): return i+distinct_characters
+
+print(f"Part One: {find_start_of(PACKET)}")
+print(f"Part Two: {find_start_of(MESSAGE)}")
